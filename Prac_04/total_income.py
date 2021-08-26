d75877a0b20b8280778e8bf26692e15e76837359
Rhys <rhys.e.sheehan@protonmail.com>
2021-08-26 12:52:25 +1000

    fix code formatting

--- Prac_04/total_income.py
+++ Prac_04/total_income.py
@@ -9,7 +9,7 @@ def main():
     number_of_months = int(input("How many months? "))
 
     for month in range(1, number_of_months + 1):
-        income = float(input("Enter income for month {}: " .format(month)))
+        income = float(input("Enter income for month {}: ".format(month)))
         incomes.append(income)
 
     print_report(incomes, number_of_months)
