6b1f3ab135119d340cf687dcf51c86e86a81f30c
Rhys <rhys.e.sheehan@protonmail.com>
2021-08-26 11:25:54 +1000

    answer lists_warmup questions

---
+++ Prac_04/lists_warmup.py
@@ -0,0 +1,21 @@
+"""Rhys Sheehan CP1404 Practical 04"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0] = 3
+# numbers[-1] = 2
+# numbers[3] = 1
+# numbers[:-1] =
+# numbers[3:4] = 1
+# 5 in numbers = True
+# 7 in numbers = False
+# "3" in numbers = False
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = 10
+
+numbers[-1] = 1
+
+numbers[:2]
+
+9 in numbers
