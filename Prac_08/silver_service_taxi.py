dd93335efc0f83ed6c47eb358c691da0d8922a2d
Rhys <rhys.e.sheehan@protonmail.com>
2021-10-12 13:37:20 +1000

    fix flagfall

--- Prac_08/silver_service_taxi.py
+++ Prac_08/silver_service_taxi.py
@@ -4,6 +4,8 @@ from Prac_08.taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
+    flagfall = 4.5
+
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.fanciness = fanciness
